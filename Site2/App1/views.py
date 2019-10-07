3deaeec368cf96d085111c862bd528ed8509e1ed
guillermo_gonzalez_orellana <guillermo.gonzalez@ericsson.com>
2019-10-07 12:49:29 -0300

    Cambio app1/views.py - index

--- Site2/App1/views.py
+++ Site2/App1/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Animal
 
 
 def index(request):
     animales = Animal.objects.all()
+    animals = []
     for animal in animales:
-        print(animal.nro, animal.name)
-    return HttpResponse("Hello, world. You're at the App1 index.")
+        animals.append({
+            animal.nro: {
+                'name': animal.name,
+                'color': animal.color,
+                'nacimiento': animal.birth,
+                'etapa': animal.stage}
+            }
+        )
+    #return HttpResponse("Hello, world. You're at the App1 index.")
+    return JsonResponse(animals, safe=False)
